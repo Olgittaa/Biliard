4e50d1645545469e380897f7d28ead31aa915a82
olga_charna <olya13771@gmail.com>
2022-10-13 12:57:09 +0200

    Initial Commit

---
+++ Biliard/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Biliard
+//
+//  Created by Ольга Чарная on 13.10.2022.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
